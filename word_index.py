7103a3337441f37be7588869da7c302aec91025f
Bhavita <Bhavita_Jain2@baylor.edu>
2023-03-03 02:52:39 -0600

    EC Assignment

--- word_index.py
+++ word_index.py
@@ -1,27 +1,7 @@
 
-'''
-myfile = open('book of John text.txt', 'r')
-read1 = myfile.read()
 
-word = 'God'
-ctr = 0
-for r in read1:
-    if r.find(word) == True:
-        ctr += 1
-print(f" {word} : {ctr}")
-
-#2
-word = "life"
-ctr = 0
-with open (r'book of John text.txt', 'r') as myfile:
-    content = myfile.read()
-    for i in content:
-        if str(word) in content:
-            ctr = ctr+1
-    print(ctr)    
 '''
-'''
-#3 Working Code!!
+#1 Working Code using list
 word = ["life", "God"]
 ctr = [0,0]
 #ctr1 = 0
@@ -38,7 +18,7 @@ print(f"{word[0]}: {ctr[0]}")
 print(f"{word[1]}: {ctr[1]}")  
 '''
 
-#4
+#2 Using dictionary
 mydict = {'Father':0, 'God':0, 'Christ':0, 'Spirit':0, 'life':0, 'man':0}
 with open ('book of John text.txt', 'r') as myfile:
     for line in myfile:
